dc69d1aeb7f6f4fba980ddb73cb0917f9c02e14c
Rehan Ahmad Rai <rehan.ahmad.rai@gmail.com>
2024-03-30 16:30:18 +0000

    add secret key

--- env.py
+++ env.py
@@ -2,3 +2,4 @@ import os
 
 os.environ.setdefault(
     "DATABASE_URL", "postgres://ihdvicwl:X2HgWK5XG3bMOFQbGpf8Xfs7HsFYBAYk@flora.db.elephantsql.com/ihdvicwl")
+os.environ.setdefault("SECRET_KEY", "<your_choice_of_secret_key>")
