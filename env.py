bf15882df2fb2a648598b088243e466341c11a22
Marcopolo1975 <140216383+Marcopolo1975@users.noreply.github.com>
2024-03-30 16:53:50 +0100

    Update env.py

--- env.py
+++ env.py
@@ -1,4 +1,4 @@
 import os
 
 os.environ.setdefault(
-    "DATABASE_URL", "<postgres://ihdvicwl:X2HgWK5XG3bMOFQbGpf8Xfs7HsFYBAYk@flora.db.elephantsql.com/ihdvicwl>")
+    "DATABASE_URL", "postgres://ihdvicwl:X2HgWK5XG3bMOFQbGpf8Xfs7HsFYBAYk@flora.db.elephantsql.com/ihdvicwl")
