b573ea1f9e4f17d1754b1b52066349974f4ddb43
Rehan Ahmad Rai <rehan.ahmad.rai@gmail.com>
2024-04-22 10:46:13 +0000

    add Addpost function

---
+++ addpost/forms.py
@@ -0,0 +1,8 @@
+from .models import AddPostRequest
+from django import forms
+
+
+class AddPostForm(forms.ModelForm):
+    class Meta:
+        model = AddPostRequest
+        fields = ('immage', 'details',)
---
+++ addpost/views.py
@@ -0,0 +1,24 @@
+from django.shortcuts import render
+from django.contrib import messages
+from .models import AddPost
+from .forms import AddPostForm
+
+
+def add_post(request):
+    
+    if request.method == "POST":
+        addpost_form = AddPostForm(data=request.POST)
+        if addpost_form.is_valid():
+            addpost_form.save()
+            messages.add_newpost(request, messages.SUCCESS, "Collaboration request received! I endeavour to respond within 2 working days.")
+    addpost = AddPost.objects.all().order_by('-updated_on').first()
+    addpost_form = AddPostForm()
+
+    return render(
+        request,
+        "addpost/addpost.html",
+        {
+            "addpost": addpost,
+            "addpost_form": addpost_form
+        },
+    )
