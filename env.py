bc97d85a4f605b9db51a00096712966110cb9943
Rehan Ahmad Rai <rehan.ahmad.rai@gmail.com>
2024-07-04 00:27:40 +0000

    remove secret credentials

--- env.py
+++
@@ -1,8 +0,0 @@
-import os
-
-os.environ.setdefault(
-    "DATABASE_URL", "postgres://ihdvicwl:X2HgWK5XG3bMOFQbGpf8Xfs7HsFYBAYk@flora.db.elephantsql.com/ihdvicwl")
-os.environ.setdefault("SECRET_KEY", "5964")
-
-os.environ.setdefault(
-    "CLOUDINARY_URL", "cloudinary://766243996956556:qwWwMOn6RCQkgwbsit7vdg1qw2w@dodutelmv")
--- explorer/settings.py
+++ explorer/settings.py
@@ -26,7 +26,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure--h2wowbq%09hj@qqd-5tj*2)-+_-nu0*8^+tctou*nidd!tas1'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
